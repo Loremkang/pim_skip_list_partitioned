6cd095fb60ba24dd5660a234b76a38e8d0d86b85
Hongbo Kang <hongbokang@icloud.com>
2022-04-28 19:13:18 +0800

    support zipf 1.2.

--- common/settings.h
+++ common/settings.h
@@ -1,13 +1,14 @@
 #pragma once
 
-// #define KHB_DEBUG_CORE
-// #define KHB_DEBUG_SPACE
-// #define KHB_CPU_DEBUG
-// #define KHB_DEBUG
+#define KHB_DEBUG_CORE
+#define KHB_DEBUG_SPACE
+#define KHB_CPU_DEBUG
+#define KHB_DEBUG
 // #define KHB_DEBUG_L3
 // #define KHB_DEBUG_TASK
 // #define KHB_INFO
-#define BLC_DETAIL
+// #define BLC_DETAIL
 #define INFO_IO_BALANCE
+#define PRINT_IO
 
 #define IRAM_FRIENDLY
